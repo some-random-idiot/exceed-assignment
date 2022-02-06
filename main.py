8afa5f08090ff0007d971786bd562e308507f299
Wha Chanunya <whachanunya@gmail.com>
2022-02-06 15:52:04 +0700

    fix everthing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 
 class Reservation(BaseModel):
     name : str
-    time: int
+    time: str
     table_number: int
     
 client = MongoClient('mongodb://localhost', 27017)
@@ -33,15 +33,23 @@ def reserve(reservation : Reservation):
 
 @app.put("/reservation/update/")
 def update_reservation(reservation: Reservation):
-    queue = collection.find_one({"table_number": reservation.table_number, 
-                                 "time": reservation.new_time})
-    if queue is None:
-        return collection.update_one({"name": reservation.name, 
-                                      "table_number": reservation.table_number}, 
-                                      { "%set": {"time": reservation.new_time}
-                                    })
+    reserve = collection.find_one({"time": reservation.time}, {"_id": 0})
+    queue = {"table_number": reservation.table_number, 
+             "name": reservation.name}
+    new_time = { "$set": {"time": reservation.time} }
+    if reserve is None:
+        collection.update_many(queue, new_time)
+        return {
+            "status": "Updated"
+        }
+    return {
+        "status": "Unfortunately, table is already reserved."
+    }
 
 @app.delete("/reservation/delete/{name}/{table_number}")
 def cancel_reservation(name: str, table_number : int):
-    reservation = {"name" : name, "table_namber": table_number}
+    reservation = {"name" : name, "table_number": table_number}
     collection.delete_one(reservation)
+    return {
+        "status": "Deleted"
+    }
